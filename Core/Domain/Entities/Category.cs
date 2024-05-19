ff3b2cdd16cb417b2eba29f6c49fe5706e619255
Alptekin EROL <81481277+AlptekinErol@users.noreply.github.com>
2024-05-20 02:09:46 +0300

    temel entityler olusturuldu ( category , product vb)

---
+++ Core/Domain/Entities/Category.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Category : EntityBase, IEntityBase
+    {
+        public Category()
+        {
+
+        }
+        public Category(int parentId, string name, int priority)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priority = priority;
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+
+        // Detail realtion with collection
+        public ICollection<Detail> Details { get; set; }
+
+        // n-n relation with Product                      example => Home Stuff (id 1) , Electronical (id 2) => user have to see Bulb Product in both Category
+        public ICollection<Product> Products { get; set; }
+    }
+}
+
+
---
+++ Core/Domain/Entities/Product.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        //public Product()
+        //{
+
+        //}
+        //public Product(string title, string description, int BrandId, decimal price, decimal discount)
+        //{
+        //    Title = title;
+        //    Description = description;
+        //    this.BrandId = BrandId;
+        //    Price = price;
+        //    Discount = discount;
+        //}
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int BrandId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+
+
+        // relation with Brand Table 
+        Brand Brand { get; set; }
+
+        // n-n relation with Category Table                  example => Home Stuff (id 1) , Electronical (id 2) => user have to see Bulb Product in both Category
+
+        public ICollection<Category> Categories { get; set; }
+
+    }
+}
